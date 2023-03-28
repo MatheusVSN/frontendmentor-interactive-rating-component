fb58c019e6af036cd58360326cfcfc1609ddf224
Matheus <matheusvsn2005@gmail.com>
2023-03-28 09:05:53 -0300

    28/03/2023

--- src/rating-component.jsx
+++ src/rating-component.jsx
@@ -1,15 +1,19 @@
 import React from "react"
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import MainPage from "./assets/pages/main"
 import ThankYou from "./assets/pages/thank-you"
 
+const PathString = "/frontendmentor-interactive-rating-component"
+
 function RatingComponentPreview() {
     return (
-        <Routes>
-            <Route path="/" element={<MainPage/>}/>
-            <Route path="/thank-you" element={<ThankYou/>}/>
-        </Routes>
+        <React.StrictMode>
+            <Routes>
+                <Route path={`${PathString}`} element={<MainPage />} />
+                <Route path={`${PathString}/thank-you`} element={<ThankYou />} />
+            </Routes>
+        </React.StrictMode>
     )
 }
 
