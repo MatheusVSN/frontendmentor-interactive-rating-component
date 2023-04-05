1f88fbd68b140c5ec073a94f1d565c6e7c728f4b
Matheus <125493278+MatheusVSN@users.noreply.github.com>
2023-04-04 21:05:27 -0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="bg-very-dark-blue h-screen grid place-content-center">
+      <Head/>
+      <body className="bg-very-dark-blue h-screen grid place-content-center p-8">
         <Main />
         <NextScript />
       </body>
