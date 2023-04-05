71f2ef3aa7e1707e43e23ab6d616ebd2c794287a
Matheus <125493278+MatheusVSN@users.noreply.github.com>
2023-04-04 21:04:44 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,16 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
-      "orange": "#fb7413",
-      "white": "#ffffff",
+      orange: "#fb7413",
+      white: "#ffffff",
       "light-grey": "#959eac",
       "medium-grey": "#7c8798",
       "dark-blue": "#252d37",
+      "dark-blue-2": "#323c49",
       "very-dark-blue": "#121417",
+    },
+    fontFamily: {
+      "overpass": ["Overpass"],
     },
   },
   plugins: [],
